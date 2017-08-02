c89cd2d6b08d13e82b0e78727bc7956e7b272cb4
poldrack <poldrack@gmail.com>
2017-08-02 13:56:32 -0700

    add links, remove formatting of obj numbers

--- syllabus/mk_syllabus.py
+++ syllabus/mk_syllabus.py
@@ -11,19 +11,22 @@ if not os.path.exists(lecturebase):
 
 syll=get_syllabus.get_syllabus()
 
-syll_columns=['Date', 'Topic', 'Reading/Assignment']
+syll_columns=['Date', 'Topic', 'Reading']
 header=[i.strip() for i in syll[0]]
 syll_colnums=[]
+header_index={}
 for i,h in enumerate(header):
     if h in syll_columns:
         syll_colnums.append(i)
+    header_index[h]=i
+
 
 content=syll[1:]
 
 outfile='index.md'
 with open(outfile,'w') as f:
     f.write('---\nlayout: default\ntitle: Psych 10: Syllabus\n---\n')
-    f.write('## Syllabus\n\n')
+    f.write('## Syllabus\n\nClick on the date for more information about each lecture\n\n')
     f.write('| '+'|'.join(syll_columns)+'|\n')
     # create separator
     sep=[]
@@ -39,7 +42,7 @@ with open(outfile,'w') as f:
                 if s[c].find('no class')>-1:
                     s[c]='**'+s[c]+'**'
                     noclass=True
-                rowcontent.append(s[c])
+                rowcontent.append(s[c].replace('\n','<br>'))
         if s[0]=='TBD':
             noclass=True
         if not noclass:
@@ -55,17 +58,26 @@ with open(outfile,'w') as f:
             lecturefile=os.path.join(lecturedir,'index.md')
             with open(lecturefile,'w') as lf:
                 lf.write('---\nlayout: default\ntitle: Psych 10: Lecture %d\n---\n'%lecturectr)
-                lf.write('# Lecture %d (%s): %s\n\n### Learning Objectives:\n'%(lecturectr,
+                lf.write('# Lecture %d (%s): %s\n\n'%(lecturectr,
                             s[0],s[1]))
-                lf.write('After this lecture, you should be able to:\n')
-                if len(s)>6:
-                    learnobj=s[6].split('\n')
+                if len(s)>header_index['Learning Objectives']:
+                    lf.write('### Learning Objectives:\nAfter this lecture, you should be able to:\n')
+                    learnobj=s[header_index['Learning Objectives']].split('\n')
                     for li,l in enumerate(learnobj):
                         if len(l)==0:
                             continue
-                        lsp=l.split(' ')
-                        l=lsp[1:]
-                        l.append('(%s)'%lsp[0].replace('.',''))
-                        l=' '.join(l)
-                        lf.write('%d. %s\n'%(li+1,l))
+                        # lsp=l.split(' ')
+                        # l=lsp[1:]
+                        # l.append('(%s)'%lsp[0].replace('.',''))
+                        # l=' '.join(l)
+                        lf.write('* %s\n'%l)
+                if len(s)>header_index['Links']:
+                    lf.write('\n### Links:\n')
+                    links=s[header_index['Links']].split('\n')
+                    for li,l in enumerate(links):
+                        lf.write('* %s\n'%l)
+
+
+
+
             lecturectr+=1
